425a34261d80fc4529da44f6e16d064c639807ee
tobia <ddger@live.com>
2018-04-24 17:47:59 +0200

    initialisierung von nussinov

--- assignment1/nussinov.py
+++ assignment1/nussinov.py
@@ -26,9 +26,24 @@ def main():
 
             if line[0] == '>':
                 continue
-
             sequence = line.strip()
 
-    print(sequence)
+    dp_dic = {}
+
+    # helper function so i don't mess up the indices
+    range1 = lambda start, end: range(start, end + 1)
+
+    for i in range(2,len(sequence)):
+        dp_dic[i][i - 1] = 0
+
+    for i in range(1,len(sequence)):
+        dp_dic[i][i] = 0
+
+
+    # recurssion
+    for i in range1(2,len(sequence)):
+        for j in range1(i,len(sequence)):
+
+
 
 main()
