fd68f7b147daf50788886be8fdfd3e4243adfcc4
tobia <ddger@live.com>
2018-04-25 16:16:19 +0200

    initialisierung von nussinov1

--- assignment1/nussinov.py
+++ assignment1/nussinov.py
@@ -10,16 +10,24 @@ def is_valid_file(parser, arg):
     else:
         return arg  # return an open file handle
 
+# helper function so i don't mess up the indices
+rangeInclusive = lambda start, end: range(start, end + 1)
+rangeExclusive = lambda start, end: range(start + 1, end)
+
 def main():
+
     parser = argparse.ArgumentParser(description='imput fasta file')
     parser.add_argument('-i', dest="filename", metavar="FILE", help='input file', required=True,type=lambda x: is_valid_file(parser, x))
 
     args = parser.parse_args()
 
+    score_AU = 1
+    score_CG = 1
+    score_GU = 1
     input_file = args.filename
 
     sequence = ''
-
+    dic_sequence = {}
     with open('input_files/supplement01/test.fasta', 'r') as file_handle:
         for line in file_handle:
             line = line.strip()
@@ -28,21 +36,45 @@ def main():
                 continue
             sequence = line.strip()
 
+    # transform seuence to dict because of indices
+    for i in rangeInclusive(1,len(sequence)):
+        dic_sequence[i] = sequence[i-1]
+
     dp_dic = {}
+    traceback_dic = {}
 
-    # helper function so i don't mess up the indices
-    range1 = lambda start, end: range(start, end + 1)
 
-    for i in range(2,len(sequence)):
-        dp_dic[i][i - 1] = 0
 
-    for i in range(1,len(sequence)):
+    # initialize with 0
+    for i in rangeInclusive(2,len(sequence)):
+        dp_dic[i][i-1] = 0
+
+    for i in rangeInclusive(1,len(sequence)):
         dp_dic[i][i] = 0
 
+    dic_score = {}
+    dic_score['A','U'] = score_AU
+    dic_score['U', 'A'] = score_AU
+    dic_score['C', 'G'] = score_CG
+    dic_score['G', 'C'] = score_CG
+    dic_score['G', 'U'] = score_GU
+    dic_score['U', 'G'] = score_GU
 
-    # recurssion
-    for i in range1(2,len(sequence)):
-        for j in range1(i,len(sequence)):
+    # initialization recursion
+    for I in rangeInclusive(2,len(sequence)):
+        for j in rangeInclusive(I,len(sequence)):
+            i = j - I + 1
+
+            case4_max = -1
+            temp_trace = {}
+            for k in rangeExclusive(i,j):
+                if dp_dic[i,k] + dp_dic[k+1,j] > case4_max:
+                    temp_trace = ((i,k),(k+1,j))
+
+            case1 = dp_dic[i + 1, j]
+            case2 = dp_dic[i, j - 1]
+            case3 = dp_dic[i + 1, j - 1] + dic_score[dic_sequence[i], dic_sequence[j]]
+
 
 
 
