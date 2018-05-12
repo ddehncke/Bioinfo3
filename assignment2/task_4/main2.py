837a0197a633214bd33f8aed0c68ccb6cbcb85d8
Daniel <ddger@live.com>
2018-05-12 02:57:35 +0200

    4 ansatz fertig

--- assignment2/task_4/main2.py
+++ assignment2/task_4/main2.py
@@ -21,22 +21,58 @@ def main():
 
     all_paths = os.listdir(args.folder)
 
-    # for filename in all_paths:
-    #     path_to_current_file = os.path.join(args.folder, filename)
-    #     parser = PDBParser()
-    #     structure = parser.get_structure('temp', path_to_current_file)
-    #
-    #
+    for filename in all_paths:
+        path_to_current_file = os.path.join(args.folder, filename)
+        parser = PDBParser()
+        structure = parser.get_structure('temp', path_to_current_file)
 
-    # phi
-    C1 = np.array([8.326, 10.351, 0.000])
-    N = np.array([9.000, 9.000, 0.000])
-    Ca = np.array([10.325, 9.000, 0.000])
-    C2 = np.array([11.096, 7.766, 0.000])
+        phi = []
+        psi = []
 
-    b_1 = N - C1
-    b_2 = Ca - N
-    b_3 = C2 - Ca
+        temp_residue = None
+
+        for model in structure:
+            for chain in model:
+                for residue in chain:
+
+                    # need to skip residues that are incorrect
+                    if 'O' not in residue or 'N' not in residue:
+                        temp_residue = None
+                        continue
+                    if temp_residue is not None:
+                        # TODO Es müssen drei residuen betrachtet werden. Einfach eine temp_residue mehr, da eh alles
+                        # auf stack
+                        phi.append(getAngle(temp_residue['C'], temp_residue['N'], residue['Ca'], residue['C']))
+                        psi.append(getAngle(temp_residue['N'], temp_residue['Ca'], residue['Ca'], residue['C']))
+
+
+
+                    # temp_dist.append(np.linalg.norm(stack_O.pop() - residue['N'].get_vector()))
+
+                    print(residue['N'].get_vector())
+
+
+
+
+
+
+
+    # A1 = np.array([8.326, 10.351, 0.000])
+    # A2 = np.array([9.000, 9.000, 0.000])
+    # A3 = np.array([10.325, 9.000, 0.000])
+    # A4 = np.array([11.096, 7.766, 0.000])
+
+    phi = getAngle(C1, N, Ca, C2)
+    psi = getAngle(N, Ca, C, N)
+
+
+
+def getAngle(A1, A2, A3, A4):
+
+
+    b_1 = A2 - A1
+    b_2 = A3 - A2
+    b_3 = A4 - A3
 
     q1 =np.cross(b_1, b_2)
     n_1 = np.cross(b_1, b_2) / np.linalg.norm(np.cross(b_1, b_2))
@@ -49,7 +85,5 @@ def main():
 
 
     print(np.degrees(-math.atan2(y, x)))
-
-    print(calc_dihedral(C1,N,Ca,C2))
 
 main()
